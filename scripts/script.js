88ee6f5f94b14d69580a945378c03a87228bf412
Julia Nikolaeva <iamenates@gmail.com>
2021-03-20 20:19:33 +0300

    add lessons

---
+++ scripts/script.js
@@ -0,0 +1,61 @@
+"use strict";
+
+// const obj = {
+//     name: 'John',
+//     age: 25,
+//     isMarried: false,
+// };
+
+// console.log(obj.name);
+// console.log(obj['name']);
+
+// let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+// console.log(arr[1]);
+
+//alert('Hello');
+
+// const result = confirm('are you here?');
+// console.log(result);
+
+// const answer = prompt('Вам есть 18,', '18');
+// console.log(typeof(answer));
+
+// const answers = [];
+
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Как ваша фамилия?', '');
+// answers[2] = prompt('Сколько вам лет?', '');
+
+// document.write(answers);
+
+// const category = 'toys';
+//console.log('https://someurl.com/' + category);
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = 'Julia';
+// console.log(`Привет ${user}`);
+
+// ОПЕРАТОРЫ в JS:
+// let incr = 10,
+//     decr = 10;
+
+// // Постфиксная:
+// // incr++;
+// // decr--;
+
+// // Префиксная:
+// // ++incr;
+// // --decr;
+
+// // console.log(incr++); // будет 10, т.к. постфиксная, сначало возвращает старое значение. А после этого уже увеличивает значение на 1.
+// // console.log(decr--); // // будет 10, т.к. постфиксная, сначало возвращает старое значение. А после этого уже уменьшает значение на 1.
+
+// console.log(++incr); // префиксная, увеличивает на 1 и возвращает.
+// console.log(--decr); // префиксная, уменьшает на 1 и возвращает.
+
+// const 
+//     isChecked = true,
+//     isClose = false,
+//     isTest = false;
+
+// console.log(isClose || isTest);
